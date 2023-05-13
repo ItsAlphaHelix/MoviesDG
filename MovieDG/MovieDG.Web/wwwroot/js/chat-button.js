6a467d4969ef5fe9ac8053298fce0340f70ea32f
ItsAlphaHelix <mitak4000@gmail.com>
2023-05-14 00:49:57 +0300

    Implementing a straightforward script for chat buttons.

--- MovieDG/MovieDG.Web/wwwroot/js/chat-button.js
+++ MovieDG/MovieDG.Web/wwwroot/js/chat-button.js
@@ -1,5 +1,8 @@
 ﻿const chatButton = document.querySelector('.chatbox__button');
 const chatContent = document.querySelector('.chatbox__support');
+const sectionElement = document.querySelector('.banner-area');
+const sectionTwoElement = document.querySelector('.ucm-area');
+
 const icons = {
     isClicked: '<img src="./images/chatbox-icon.svg" />',
     isNotClicked: '<img src="./images/chatbox-icon.svg" />',
@@ -7,3 +10,12 @@ const icons = {
 const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
 chatbox.display();
 chatbox.toggleIcon(false, chatButton);
+
+
+sectionElement.addEventListener('click', function () {
+    chatContent.classList.remove('chatbox--active');
+});
+
+sectionTwoElement.addEventListener('click', function () {
+    chatContent.classList.remove('chatbox--active');
+});
